33c8d8efdabd0643b9f51fd22460a71620839ef7
Jashan Kishore <jashan.kishore@gmail.com>
2024-06-04 21:35:04 +1000

    Initial Commit

---
+++ SimpleCounter/SimpleCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCounterApp.swift
+//  SimpleCounter
+//
+//  Created by Jashan kishore on 4/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
